c5670f741227da50852075e26e4f3a9971e2e5f8
Hector Luis Rodriguez Rico <hrodriguezr1@unicartagena.edu.co>
2026-09-17 00:15:05 -0500

    feat: definir firmas en el puerto UsuarioRepository y finalizar el CRUD completo

--- src/main/java/dominio/puertos/UsuarioRepository.java
+++ src/main/java/dominio/puertos/UsuarioRepository.java
@@ -6,4 +6,7 @@ import java.util.List;
 public interface UsuarioRepository {
     void guardar(Usuario usuario);
     List<Usuario> listarTodos();
+    Usuario obtenerPorId(int id);
+    void actualizar(Usuario usuario);
+    void eliminar(int id);
 }
